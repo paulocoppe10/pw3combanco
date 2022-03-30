3f1cdb7f910fe0706a5a8dec5511a418b99fe612
paulocoppe10 <paulocoppe10@gmail.com>
2022-03-30 15:00:58 -0300

    resolvendo problema migration e criando seed e model pessoas

---
+++ app/Database/Seeds/Pessoas.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class Pessoas extends Seeder
+{
+    public function run()
+    {
+        $nome = ['Maria','ana','pedro','joao','marcos'];
+        for($x = 0; $x < 5; $x++){
+            $data = [
+                'nome' => $nome[$x],
+                'profissao'    => 'progamador',
+                'idade'=> 20
+            ];
+            $this->db->table('tb_pessoas')->insert($data);
+        }
+
+
+       
+    }
+}
